c33f28cb2df566262573acfeb0216de43f64dab6
ankit-gautam23 <ankit.gautam23@gmail.com>
2023-05-18 02:21:54 +0530

    Some changes and refactoring

--- background.js
+++ background.js
@@ -16,3 +16,36 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     return true;
   });
   
+  function checkKeywords(keywords, url) {
+    const query = url.toLowerCase();
+  
+    for (const keyword of keywords) {
+      // Your keyword checking logic goes here
+      // For example, you can use a regular expression to check if the keyword is present in the search query
+      const regex = new RegExp(`\\b${keyword}\\b`, 'i');
+  
+      if (regex.test(query)) {
+        // The keyword was found in the search query
+        // sendEmail(keyword);
+        // sendTeamsMessage(keyword);
+        console.log(keyword);
+      }
+    }
+  }
+  
+//   // Event listener callback for omnibox input entered
+// function onInputEntered(sentence) {
+//   const keywordList = ['example1', 'example2', 'example3'];
+//   checkKeywords(keywordList, sentence);
+// }
+
+// // Event listener for omnibox input entered
+// chrome.omnibox.onInputEntered.addListener(onInputEntered);
+
+  // Event listener for tab updates
+  chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    if (changeInfo.url) {
+      const keywordList = ['job', 'open to work', 'remote jobs'];
+      checkKeywords(keywordList, changeInfo.url);
+    }
+  });
